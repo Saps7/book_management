b1d27f361c095e9263190b3bd815126fa5f45cde
Saps7 <saptarshisg.2000@gmail.com>
2023-11-09 17:11:14 +0530

    Small change

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${3000}!`);
+  console.log(`Server is running on port ${PORT}!`);
 });
 
 app.use("/api/book", bookRouter);
